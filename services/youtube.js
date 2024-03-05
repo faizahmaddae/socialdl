a50a362907ed6980c563c18152d110da17d38d97
faizahmaddae <faizahmad.daiee@gmail.com>
2024-03-04 21:58:16 -0500

    new chagnes by faizo

--- services/youtube.js
+++ services/youtube.js
@@ -46,6 +46,7 @@ const headers = {
     // "Sec-Fetch-Site": "cross-site",
     // "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
     // "X-CSRF-TOKEN": csrfToken,
+    "X-Forwarded-For": "73.133.57.109"
 };
 
 async function downloadVideo(res, url) {
