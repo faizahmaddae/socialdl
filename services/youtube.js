1dadaa6b9581424203ac34901ffcf97c2c6cbe49
faizahmaddae <faizahmad.daiee@gmail.com>
2024-03-04 21:51:40 -0500

    Update youtube.js

--- services/youtube.js
+++ services/youtube.js
@@ -44,7 +44,7 @@ const headers = {
     // "Sec-Fetch-Dest": "empty",
     // "Sec-Fetch-Mode": "cors",
     // "Sec-Fetch-Site": "cross-site",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    // "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
     // "X-CSRF-TOKEN": csrfToken,
 };
 
