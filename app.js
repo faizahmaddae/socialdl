4d889e71240eb6dff6671a6611fb38ddd66c8ad3
faizahmaddae <faizahmad.daiee@gmail.com>
2024-03-09 10:53:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./api');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // clear console
 console.clear();
