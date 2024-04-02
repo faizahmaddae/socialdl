2bf5a9bc46351570231323f60b6dac27cdd0d202
faizahmaddae <faizahmad.daiee@gmail.com>
2024-04-02 15:38:17 -0400

    Update cobalt.js

--- services/cobalt.js
+++ services/cobalt.js
@@ -14,20 +14,38 @@ const baseUrl = 'https://co.wuk.sh/api/json';
 
 
 
+function getYouTubeThumbnailUrl(youtubeUrl) {
+    // Extract the video ID using a regular expression
+    const videoIdMatch = youtubeUrl.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
+    
+    // Check if a video ID was found
+    if (videoIdMatch && videoIdMatch[1]) {
+      // Construct the URL for the high-quality thumbnail
+      const videoId = videoIdMatch[1];
+      return `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+    } else {
+      // Return a message or null if the video ID could not be extracted
+      return null;
+    }
+  }
+
 async function downloadFromYoutube(res, url) {
 
     // extract video ID
-    const parsedUrl = new URL(url);
-    const videoId = parsedUrl.searchParams.get('v');
+    // const parsedUrl = new URL(url);
+    // const videoId = parsedUrl.searchParams.get('v');
 
     // https://i.ytimg.com/vi/<--Video ID-->/sddefault.jpg
     // https://i.ytimg.com/vi/4i16Ako-89A/sddefault.jpg
+    // https://i.ytimg.com/vi/onHeWRqkPsM/sddefault.jpg
 
     const params = {
         'url': url,
         'vCodec': 'mp4',
         'vQuality': '1080',
     };
+
+    const thumbnail = getYouTubeThumbnailUrl(url);
 
     let data = [];
 
@@ -37,7 +55,7 @@ async function downloadFromYoutube(res, url) {
             data.push({
                 'title': null,
                 'badge': null,
-                'thumbnail': `https://i.ytimg.com/vi/${videoId}/sddefault.jpg`,
+                'thumbnail': thumbnail,
                 'download_url': response.data.url,
                 'is_video': true,
             });
